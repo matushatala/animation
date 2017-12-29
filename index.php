99e1daa6c4acaa23925f5806e7364477ef0dd3a3
Matus Hatala <34936502+matushatala@users.noreply.github.com>
2017-12-29 10:48:50 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="chrome=1,IE=edge" />
+	<title>Animation 😀</title>
+	<style>
+		html {
+			height:100%;
+		}
+		body {
+			background-color:#000000;
+			margin:0;
+			height:100%;
+		}
+	</style>
+	
+	<meta name="viewport" content="user-scalable=yes, initial-scale=1.0, width=320" />
+	<meta name="description" content="Hello from Matus Hatala."/>
+   <link rel="canonical" href="https://matushatala.github.io/animation/" />
+   <meta property="og:locale" content="en_US" />
+   <meta property="og:type" content="website" />
+   
+   <script>
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+     })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+       ga('create', 'UA-105998848-1', 'auto');
+       ga('send', 'pageview');
+   </script>
+
+  </head>
+  <body>
+	<p style="color: #fff">Hi</p>
+  </body>
+</html>
